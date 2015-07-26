f0ae217a5c66bf8253aceda645cf0171370198cb
Giant Elk <flyinghorsedancing@gmail.com>
2015-07-26 15:43:21 -0400

    Better indentation.

--- app.jsx
+++ app.jsx
@@ -8,60 +8,60 @@ var RouteHandler = Router.RouteHandler;
 var Link = Router.Link;
 
 
-var PageOne = React.createClass({	
-	render: function() {		
+var PageOne = React.createClass({ 
+  render: function() {    
 
-		var showListHTML = [];
+    var showListHTML = [];
 
-		for (var i = 0; i <= 100; i++) {
-			showListHTML[i] = (
-				<li>
-					"Run to the hills, run for your lives. Da dana na nana na na na!"
-				</li>
-			);
-		};
+    for (var i = 0; i <= 100; i++) {
+      showListHTML[i] = (
+        <li>
+          "Run to the hills, run for your lives. Da dana na nana na na na!"
+        </li>
+      );
+    };
 
-		return (
-			<div>
-				Page One text.
-				{showListHTML}
-			</div>
-		);
-	}
+    return (
+      <div>
+        Page One text.
+        {showListHTML}
+      </div>
+    );
+  }
 });
 
 
-var PageTwo = React.createClass({	
-	render: function() {		
-		return (
-			<div>
-				Page Two text.	
-			</div>
-		);
-	}
+var PageTwo = React.createClass({ 
+  render: function() {    
+    return (
+      <div>
+        Page Two text.  
+      </div>
+    );
+  }
 });
 
 
-var App = React.createClass({	
-	render: function() {		
-		return (
-			<div>
-				<div className="my-header-menu">
-					<Link to="pageone">PageOne</Link> ---
-					<Link to="pagetwo">PageTwo</Link>
-				</div>
+var App = React.createClass({ 
+  render: function() {    
+    return (
+      <div>
+        <div className="my-header-menu">
+          <Link to="pageone">PageOne</Link> ---
+          <Link to="pagetwo">PageTwo</Link>
+        </div>
 
-				<p />
+        <p />
 
-		        <div>
-		          <RouteHandler {...this.state} {...this.props} />
-		        </div>	
+            <div>
+              <RouteHandler {...this.state} {...this.props} />
+            </div>  
 
-				<p />
-	    		PAGE FOOTER		
-			</div>
-		);
-	}
+        <p />
+          PAGE FOOTER   
+      </div>
+    );
+  }
 });
 
 
